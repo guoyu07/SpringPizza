7cab7bd4a85e9fca4996514457d47fd213755caa
Craig <wallsc@Macintosh-5.home>
2010-01-26 21:25:56 -0600

    Renamed flows such that the subflow version is now the main version. Also added check for delivery area.

--- src/main/java/com/springinaction/pizza/flow/PizzaFlowActions.java
+++ src/main/java/com/springinaction/pizza/flow/PizzaFlowActions.java
@@ -1,5 +1,8 @@
 package com.springinaction.pizza.flow;
 
+import static com.springinaction.pizza.domain.PaymentType.*;
+import static org.apache.log4j.Logger.*;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,15 +13,15 @@ import com.springinaction.pizza.domain.Customer;
 import com.springinaction.pizza.domain.Order;
 import com.springinaction.pizza.domain.Payment;
 import com.springinaction.pizza.domain.PaymentDetails;
-import com.springinaction.pizza.domain.PaymentType;
 import com.springinaction.pizza.service.CustomerNotFoundException;
 import com.springinaction.pizza.service.CustomerService;
 
 @Component
 public class PizzaFlowActions {
-  private static final Logger LOGGER = Logger.getLogger(PizzaFlowActions.class);
+  private static final Logger LOGGER = getLogger(PizzaFlowActions.class);
   
-   public Customer lookupCustomer(String phoneNumber) throws CustomerNotFoundException {     
+   public Customer lookupCustomer(String phoneNumber) 
+         throws CustomerNotFoundException {     
       Customer customer = customerService.lookupCustomer(phoneNumber);
       if(customer != null) {        
         return customer;
@@ -28,12 +31,12 @@ public class PizzaFlowActions {
    }
    
    public void addCustomer(Customer customer) {
-      LOGGER.error("-----!!!--------->  " + customer.getName());
+      LOGGER.warn("TODO: Flesh out the addCustomer() method.");
    }
 
    public Payment verifyPayment(PaymentDetails paymentDetails) {
      Payment payment = null;
-     if(paymentDetails.getPaymentType() == PaymentType.CREDIT_CARD) {
+     if(paymentDetails.getPaymentType() == CREDIT_CARD) {
        payment = new CreditCardPayment();
      } else {
        payment = new CashOrCheckPayment();
@@ -43,7 +46,12 @@ public class PizzaFlowActions {
    }
    
    public void saveOrder(Order order) {
-     
+      LOGGER.warn("TODO: Flesh out the saveOrder() method.");
+   }
+   
+   public boolean checkDeliveryArea(String zipCode) {
+     LOGGER.warn("TODO: Flesh out the checkDeliveryArea() method.");
+     return "75075".equals(zipCode);
    }
 
    @Autowired
